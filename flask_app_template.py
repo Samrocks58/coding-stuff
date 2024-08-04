086337b64afc888890fc1695e9cc60e599a5bc93
Samrocks58 <60971882+Samrocks58@users.noreply.github.com>
2024-08-04 18:09:24 -0400

    Update flask_app_template.py

--- flask_app_template.py
+++ flask_app_template.py
@@ -7,86 +7,8 @@ app = Flask(__name__)
 def hello():
     return "IT WORKS!!"
 
-invalidEnds = {}
-with open("Results2.txt", 'r') as file:
-    lines = file.readlines()
-    invalid = lines.index("Invalid Endpoints:\n")
-    parking = lines.index("Parked Sites:\n")
-    invalidLines = lines[invalid+1:parking]
-    invalidEnds = []
-    for l in invalidLines:
-        if "{" in l:
-            invalidEnds.append(eval(l.replace("\n", "")))
-            
-def make_txt():
-    global invalidEnds    
-    hzs = []
-    sortedLines = []
-    txt = ""
 
-    for d in invalidEnds:
-        hzName = d['Hosted Zone Name']
-        if not (hzName in hzs):
-            hzs.append(hzName)
-    
-    hzs.sort()            
-    for h in hzs:
-        for d in invalidEnds:
-            if d['Hosted Zone Name'] == h:
-                sortedLines.append(str(d))
-                
-    for s in sortedLines:
-        if "{" in s:
-            d = eval(s.replace("\n", ""))
-            lineText = ""
-            
-            if d['Hosted Zone Name'][-1] == ".":
-                d['Hosted Zone Name'] = d['Hosted Zone Name'][:-1]
-                
-            if hzName != d['Hosted Zone Name']:
-                hzName = d['Hosted Zone Name']
-                lineText += "\n\n\t\t\t "
-                lineText += hzName + ":"
-                
-            if d['Entry'][-1] == ".":
-                d['Entry'] = d['Entry'][:-1]
-                
-            if "https://" in txt:
-                txt = txt.replace("https://", "")
-            # d['Entry'] = d['Entry'].removeprefix("https://")
-            d['Entry'] = " " + d['Entry']
-            lineText += "\n" + d['Entry'] + ": "
-                
-            first = True
-            for key in list(d.keys()):
-                if not first:
-                    lineText += ", "
-                if d[key] and type(d[key]) != str:
-                    lineText += key
-                    first = False
-            
-            txt += lineText
-
-    while txt.removeprefix("\n") != txt:
-        txt = txt.removeprefix("\n")
-        
-    txt = f"\t\tTotal Number of URLS: {len(invalidEnds)}\n\n" + txt
-    
-    return txt
-            
-txt = make_txt()
-
-@app.route('/reinvoke', methods=['POST', 'GET'])
-def reinvoke():
-    return "WOW!"
-    if not (request.form['token'] == os.environ['SLACK_VERIFICATION_TOKEN']):
-        abort(400)
-
-    return jsonify(
-        response_type='in_channel',
-        text=''
-    )
-    
+      
 @app.route('/getExpiresSoon', methods=['POST', 'GET'])
 def getExpiresSoon():
     counter = 0
