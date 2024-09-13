656e74cbe4ca5f25504cf84894de31f399ee8c99
Samrocks58 <60971882+Samrocks58@users.noreply.github.com>
2024-09-13 14:28:53 -0400

    whatever

--- doggy.py
+++ doggy.py
@@ -1,15 +1,37 @@
 import requests, cv2, shutil, os
+import numpy as np
 response=requests.get("https://dog.ceo/api/breeds/image/random")
 response=response.json()
 if response["status"] == "success":
     link=response["message"]
-    filename=link.split("/")[-1]
     picture=requests.get(link, stream=True)
 else:
-    quit()  
-with open(filename,'wb') as f:
-    shutil.copyfileobj(picture.raw, f)
-image=cv2.imread(filename)
+    quit()
+
+image = np.asarray(bytearray(picture.content), dtype="uint8") 
+image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+height = image.shape[0]
+width = image.shape[1]
+
+# print([height, width])
+# print(image.shape)
+
+max_size = 600
+if height > max_size or width > max_size:
+    bigNum = max(height, width)
+    if bigNum == height:
+        width = round(width * max_size/height)
+        height = max_size
+    else:
+        height = round(height * max_size/width)
+        width = max_size
+
+print([height, width])
+print(image.shape)
+
+image = cv2.resize(image, (width, height))
+
 prev_char=''
 cur_char=''
 while True:
@@ -23,7 +45,10 @@ while True:
     prev_char = c
     if cur_char == ord('q'):
         break
+    try:
+        cv2.getWindowProperty('doggo', 0)
+    except Exception:
+        break
     cur_char=''
 cv2.destroyAllWindows()
-os.remove(filename)
 quit()
