a090d9744be2ae10d02c4fbb61058796740a9c6a
Samrocks58 <60971882+Samrocks58@users.noreply.github.com>
2024-10-20 20:30:10 -0400

    Update doggy.py

--- doggy.py
+++ doggy.py
@@ -1,39 +1,41 @@
-import requests, cv2, shutil, os
+import requests, cv2, os
 import numpy as np
-response=requests.get("https://dog.ceo/api/breeds/image/random")
-response=response.json()
-if response["status"] == "success":
-    link=response["message"]
-    picture=requests.get(link, stream=True)
-else:
-    quit()
 
-image = np.asarray(bytearray(picture.content), dtype="uint8") 
-image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-
-height = image.shape[0]
-width = image.shape[1]
-
-# print([height, width])
-# print(image.shape)
-
-max_size = 600
-if height > max_size or width > max_size:
-    bigNum = max(height, width)
-    if bigNum == height:
-        width = round(width * max_size/height)
-        height = max_size
+def Restart():
+    global image
+    response=requests.get("https://dog.ceo/api/breeds/image/random")
+    response=response.json()
+    if response["status"] == "success":
+        link=response["message"]
+        picture=requests.get(link, stream=True)
     else:
-        height = round(height * max_size/width)
-        width = max_size
+        quit()
 
-print([height, width])
-print(image.shape)
+    image = np.asarray(bytearray(picture.content), dtype="uint8") 
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
 
-image = cv2.resize(image, (width, height))
+    height = image.shape[0]
+    width = image.shape[1]
 
+    max_size = 600
+    if height > max_size or width > max_size:
+        bigNum = max(height, width)
+        if bigNum == height:
+            width = round(width * max_size/height)
+            height = max_size
+        else:
+            height = round(height * max_size/width)
+            width = max_size
+
+    # print([height, width])
+    # print(image.shape)
+
+    image = cv2.resize(image, (width, height))
+
+Restart()
 prev_char=''
 cur_char=''
+
 while True:
     cv2.imshow("doggo", image)
     c = cv2.waitKey(1)
@@ -45,6 +47,8 @@ while True:
     prev_char = c
     if cur_char == ord('q'):
         break
+    elif cur_char == ord('r'):
+        Restart()
     try:
         cv2.getWindowProperty('doggo', 0)
     except Exception:
